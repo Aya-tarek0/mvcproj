23d04bad6dacb0ffd006534979e6ba82c21e2924
Aya-tarek0 <ayatttarek@gmail.com>
2025-03-24 21:44:19 +0200

    ..

--- obj/Debug/net8.0/mvcproj.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvcproj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvcproj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6873dcbdb686519df3c154d07e37c67bd6f5cdb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d7640f5acc80b99d562c733e818622b87ab5ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvcproj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvcproj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
